fdf1cb751b0aa600c15197421593f5dcabb7793c
Liang <lllwsdnh@gmail.com>
2023-11-13 10:27:25 -0500

    Update BestTimetoBuyandSellStock.java

--- src/main/java/com/example/helloworld/dp/easy/BestTimetoBuyandSellStock.java
+++ src/main/java/com/example/helloworld/dp/easy/BestTimetoBuyandSellStock.java
@@ -1,2 +1,13 @@
-package com.example.helloworld.dp.easy;public class BestTimetoBuyandSellStock {
+package com.example.helloworld.dp.easy;
+
+public class BestTimetoBuyandSellStock {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int curMin = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - curMin);
+            curMin = Math.min(curMin, prices[i]);
+        }
+        return max;
+    }
 }
